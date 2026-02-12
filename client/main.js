986cdf21406c657b80d14cae64d99bee00e1efd1
RFerreiraN <ricardevferreira@gmail.com>
2026-02-12 15:30:04 +0100

    conectamos con turso

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
 import { io } from 'https://cdn.socket.io/4.7.2/socket.io.esm.min.js'
 
-const socket = io()
+const socket = io({
+  auth: {
+    serverOffset: 0
+  }
+})
 const form = document.getElementById('form')
 const input = document.getElementById('input')
 
@@ -15,8 +19,9 @@ form.addEventListener('submit', (e) => {
 
 const mensajes = document.getElementById('mensajes')
 
-socket.on('chat message', (msg) => {
+socket.on('chat message', (msg, serverOffset) => {
   const li = document.createElement('li')
   li.textContent = msg
   mensajes.appendChild(li)
+  socket.auth.serverOffset = serverOffset
 })
--- server/index.js
+++ server/index.js
@@ -2,12 +2,30 @@ import express from 'express'
 import logger from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
+
+dotenv.config()
 
 const PORT = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: true
+})
+
+const db = createClient({
+  url: process.env.DB_URL,
+  authToken: process.env.DB_AUTH_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS mensajes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  )
+`)
 
 io.on('connection', (socket) => {
   console.log('un usuario se ha conectado!!')
@@ -16,8 +34,18 @@ io.on('connection', (socket) => {
     console.log('Un usuario se ha desconectado!!')
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO mensajes (content) VALUES (:msg)',
+        args: { msg }
+      })
+    } catch (error) {
+      console.error(error)
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString())
   })
 })
 
